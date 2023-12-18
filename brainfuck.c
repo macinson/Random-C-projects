930ce8b8976bb0eaf2cfa7e078eaa7d628eb9f3d
macinson <macinson.kozik@gmail.com>
2023-12-18 23:25:20 +0100

    Somehow not working barainfuck

---
+++ brainfuck.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    // if(argc < 2){ 
+    //     printf("Pass arguments bro!");
+    //     return 0;
+    // }
+
+    char *tape = calloc(30000, 1);
+    tape += 1000;
+    int *stack = calloc(30000, 4);
+    int pos = 0;
+    FILE *pF = fopen("brainfuckTesting.txt", "r");
+    char buffer;
+    while ((buffer = fgetc(pF)) != EOF)
+    {
+        switch (buffer)
+        {
+        case '+':
+            (*tape)++;
+            break;
+        case '-':
+            (*tape)--;
+            break;
+        case '>':
+            tape++;
+            break;
+        case '<':
+            tape--;
+            break;
+        case '.':
+            printf("%c", *tape);
+            break;
+        case ',':
+            scanf("%c", tape);
+            break;
+        case '[':
+            if ((*tape) == 0)
+            {
+                int count = 0;
+                while ((buffer = fgetc(pF)) != ']' || count != 0)
+                {
+                    if (buffer == ']')
+                        count--;
+                    else if (buffer == '[')
+                        count++;
+                    pos++;
+                }
+            }
+            else
+            {
+                *stack = pos;
+                stack += 4;
+            }
+            break;
+        case ']':
+            stack-=4;
+            if ((*tape) != 0)
+            {
+                pos = (*stack)-1;  
+
+                fseek(pF, pos+1, SEEK_SET);
+            }
+            break;            
+        }
+        pos++;
+    }
+    fclose(pF);
+    return 0;
+}
---
+++ test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void chuj(char[]);
+
+int main(){
+
+    return 0;
+}
+
+void chuj(char d[]){
+    printf("%s",d);
+}
